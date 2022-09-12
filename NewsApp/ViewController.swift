bb8d7f81d0058fa2358c8e0cd6cd1bcf93ce793a
hasanuysaal <hsnuysaal@icloud.com>
2022-09-12 16:07:05 +0300

    add tableView and tableViewCell to show news

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsCell
+        cell.storyLabel.text = "Hikaye"
+        cell.titleLabel.text = "Başlık"
+        return cell
+        
+    }
 
 }
 
